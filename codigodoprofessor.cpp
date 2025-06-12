5b55c7a7bdf269ba8a1a017bdd7736ca1aeead03
NicolasCerezolli <nicolascerezollidecarvalho@gmail.com>
2025-06-11 21:45:03 -0300

    Update codigodoprofessor.cpp

--- codigodoprofessor.cpp
+++ codigodoprofessor.cpp
@@ -1,1 +1,12 @@
+void setup() {
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
 
+void loop() {
+
+  int velocidade = analogRead(A0)/4;
+  analogWrite(10, velocidade);
+  analogWrite(11, 0);
+}
